08f430cd1cf823bad22db86585df10fbcd169c53
Joshua Uzell <joshuauzell@gmail.com>
2023-10-26 12:50:52 +0100

    Recreated Ionic Project inside front end folder

--- repo/src/front-end/fypProject/capacitor.config.ts
+++ repo/src/front-end/fypProject/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fypSWProject',
+  appName: 'fypProject',
   webDir: 'www',
   server: {
     androidScheme: 'https'
