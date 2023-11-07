aa72a89841f455ee32d6ce67ad0979396be6e949
Joshua Uzell <joshuauzell@gmail.com>
2023-11-07 21:29:45 +0000

    Created basic Register page

---
+++ repo/src/front-end/fypProject/src/app/register/register.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goToProfileDetails() {
+    // Here you'd typically check the credentials, perhaps with a service that communicates with your Flask backend.
+    if(this.email && this.password && this.confirmPassword) {
+      // If credentials are valid, navigate to your dashboard or main page
+      if (this.password === this.confirmPassword) {
+        // If credentials are valid, navigate to your dashboard or main page
+        // this.router.navigate(['/profile-details']);
+        alert('Success!');
+      } else {
+        alert('Passwords do not match.');
+      }
+    } else {
+      alert('Please enter valid credentials.');
+    }
+  }
+
+}
