a6ce059cc7f89d7fd485b87882ad97e9dcfb00d3
Jose Vitor Pieroni <vitorpieroni1@gmail.com>
2023-12-07 17:03:55 +0000

    Added Driver model

--- repo/src/back-end/database.py
+++ repo/src/back-end/database.py
@@ -1,8 +1,11 @@
-from pymongo import MongoClient # Database services
+from pymongo import MongoClient
+from driver import Driver # Driver model
 from user import User # User model
 from dotenv import load_dotenv # Loading environment variables
 import os
-load_dotenv()
+load_dotenv()  # Load environment variables
+
+# String connection and database name
 # String connection and database name
 URI = os.getenv("MONGO_URI")
 MONGO_DATABASE = "ShareWay"
@@ -44,6 +47,27 @@ def add_user(user, client):
         users_collection.insert_one(user_data)
         print(f"User {user.name} added successfully.")
 
+# Adding user to Database
+def add_driver(driver, client):
+    if not client:
+        return
+
+    with client as db_client:
+        db = db_client[MONGO_DATABASE]
+        drivers_collection = db.Drivers
+
+        driver_data = {
+            "date_of_issue": driver.date_of_issue,
+            "date_of_expiry": driver.date_of_expiry,
+            "license_number": driver.license_number,
+            "license_picture": driver.license_picture,
+            "car_plate": driver.car_plate,
+            "make_and_model": driver.make_and_model,
+        }
+
+        drivers_collection.insert_one(driver_data)
+        print(f"User {driver.license_number} added successfully.")
+
 if __name__ == "__main__":
     # Example of creating and adding a user
     new_user = User(
@@ -59,5 +83,15 @@ if __name__ == "__main__":
         hobbies=["Reading", "Traveling", "Photography"]
     )
 
+    new_driver = Driver(
+        date_of_issue="2022-01-01",
+        date_of_expiry="2027-01-01",
+        license_number="ABC123",
+        license_picture="path/to/license_picture.jpg",
+        car_plate="XYZ123",
+        make_and_model="Toyota Camry"
+    )
+
     database_client = connect_to_database()
     add_user(new_user, database_client)
+    add_driver(new_driver, database_client)
---
+++ repo/src/back-end/driver.py
@@ -0,0 +1,8 @@
+class Driver:
+    def __init__(self, date_of_issue, date_of_expiry, license_number, license_picture, car_plate, make_and_model):
+        self.date_of_issue = date_of_issue
+        self.date_of_expiry = date_of_expiry
+        self.license_number = license_number
+        self.license_picture = license_picture
+        self.car_plate = car_plate
+        self.make_and_model = make_and_model
