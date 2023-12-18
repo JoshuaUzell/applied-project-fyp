46aaf271cd76c316beb1c5710d8b790be77654fe
Joshua Uzell <joshuauzell@gmail.com>
2023-12-18 01:32:15 +0000

    Added a temporary list of rides to the inwards section

--- repo/src/front-end/fypProject/src/app/home/home.page.ts
+++ repo/src/front-end/fypProject/src/app/home/home.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  showInwardRides = false;
+  inwardRides = [
+    { name: 'Mary R.', from: 'Athenry, G', availableSpace: '3/3', image: 'assets/images/woman-1.jpg'},
+    { name: 'Alfred T.', from: 'Ballinasloe, G', availableSpace: '0/1', image: 'assets/images/man-glasses.jpg'},
+    { name: 'Matilda S.', from: 'Barna, G', availableSpace: '1/4', image: 'assets/images/woman-2.jpg'},
+    { name: 'Dora D.', from: 'Clifden, G', availableSpace: '2/2', image: 'assets/images/woman-3.jpg'},
+  ];
   constructor() {}
 
 }
