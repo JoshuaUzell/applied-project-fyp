68f50c3576235cf9adfb4998a03be75f7bdbef55
Joshua Uzell <joshuauzell@gmail.com>
2024-02-05 16:42:52 +0000

    Implemented functionality for 'apply for booking' button

--- repo/src/front-end/fypProject/src/app/booking/booking.page.ts
+++ repo/src/front-end/fypProject/src/app/booking/booking.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+ 
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController, public navController: NavController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Booking Successful!',
+      message: 'You have successfully booked a ride with this driver.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+    this.navController.navigateBack('/home');
   }
 
 }
