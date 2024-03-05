6f94193f65c7092e83cb593434f28239f2b6945c
Joshua Uzell <joshuauzell@gmail.com>
2024-03-05 09:38:52 +0000

    Created a new Data Service for backend communication

--- repo/src/front-end/fypProject/src/app/login/login.page.ts
+++ repo/src/front-end/fypProject/src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service'; // Adjust the path as necessary
+
 
 @Component({
   selector: 'app-login',
@@ -10,14 +12,25 @@ export class LoginPage {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private dataService: DataService) {}
 
+  //NOTE TO SELF: NEED TO CHECK IF EMAIL AND PASSWORD ARE THE SAME AS THE ONES IN THE DATABASE
   login() {
     if(this.email && this.password) {
       this.router.navigate(['/home']);
     } else {
       alert('Please enter valid credentials.');
     }
+  }
+
+  //Loads data from the backend
+  loadData() {
+    // NOTE TO SELF: NEED PROPER API FROM JOSE
+    const apiUrl = 'http://yourbackendendpoint/api/data'; 
+    this.dataService.getData(apiUrl).subscribe(data => {
+      console.log(data);
+      // Handle your data here
+    });
   }
 
   register() {
