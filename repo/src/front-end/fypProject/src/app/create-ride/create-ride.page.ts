fdc12635ed9d21e35970a01ab19c98627e704888
Joshua Uzell <joshuauzell@gmail.com>
2024-04-22 10:08:58 +0100

    Created alerts for user when they either leave the create ride page or cancel the ride

--- repo/src/front-end/fypProject/src/app/create-ride/create-ride.page.ts
+++ repo/src/front-end/fypProject/src/app/create-ride/create-ride.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { IDatabaseInterface, IRide } from '../database.interface';
+import { DATABASE_SERVICE_TOKEN } from '../mockDatabase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-ride',
@@ -7,6 +10,7 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./create-ride.page.scss'],
 })
 export class CreateRidePage {
+  ride: IRide;
 
   //Ride fields
   numberOfSeats: number;
@@ -19,7 +23,7 @@ export class CreateRidePage {
   meetUpLocations: string[] = ['Front Entrance', 'Nothern Entrance', 'Southern Entrance', 'West Entrance'];  // Example locations
   
 
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController, @Inject(DATABASE_SERVICE_TOKEN) private databaseInterface: IDatabaseInterface, private router: Router) {}
 
   async confirmRide() {
     const alert = await this.alertController.create({
@@ -44,10 +48,57 @@ export class CreateRidePage {
     await alert.present();
   }
 
+  async cancelRide() {
+    const alert = await this.alertController.create({
+      header: 'Confirm Cancellation',
+      message: 'Are you sure you want to cancel this ride?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.databaseInterface.cancelRide(this.ride.id, this.ride.direction);
+            this.router.navigateByUrl('/home');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async confirmLeavePage() {
+    const alert = await this.alertController.create({
+      header: 'Leave Page?',
+      message: 'If you leave, the ride will be canceled. Continue?',
+      buttons: [
+        {
+          text: 'Stay',
+          role: 'cancel'
+        },
+        {
+          text: 'Leave',
+          handler: () => {
+            this.cancelRide();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
   createRide() {
     //Need logic here to create the ride..
+    this.databaseInterface.addRide(this.ride);
+    this.showProgressBar();
 
     console.log('Ride created:', this.numberOfSeats, this.direction, this.meetUpLocation);
+  }
+
+  showProgressBar() {
+    // Logic to display a progress bar indicating the search for riders
   }
 
 }
