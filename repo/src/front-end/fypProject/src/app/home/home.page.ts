f11ab1facfa1576e13d34c61f5c6d914c7e33256
Joshua Uzell <joshuauzell@gmail.com>
2024-04-24 10:05:23 +0100

    Implemented boolean logic in home page that makes the home contents dissapear once one of the buttons is clicked

--- repo/src/front-end/fypProject/src/app/home/home.page.ts
+++ repo/src/front-end/fypProject/src/app/home/home.page.ts
@@ -10,28 +10,8 @@ import { IDatabaseInterface } from '../database.interface';
 })
 export class HomePage implements OnInit{
   
+  makeHomepageContentsVisible = true;
 
-  
-  ////////////////////////
-  ////OLD CODE BELOW//////
-  ///////////////////////
-
-  // showInwardRides = false;
-  // showOutwardRides = false;
-  
-  // inwardRides = [
-  //   { name: 'Mary R.', from: 'Athenry, G', availableSpace: '3/3', image: 'assets/images/woman-1.jpg'},
-  //   { name: 'Alfred T.', from: 'Ballinasloe, G', availableSpace: '0/1', image: 'assets/images/man-glasses.jpg'},
-  //   { name: 'Matilda S.', from: 'Barna, G', availableSpace: '1/4', image: 'assets/images/woman-2.jpg'},
-  //   { name: 'Dora D.', from: 'Clifden, G', availableSpace: '2/2', image: 'assets/images/woman-3.jpg'},
-  // ];
-
-  // outwardRides = [
-  //   { name: 'Johnny R.', from: 'Lettermore, G', availableSpace: '2/3', image: 'assets/images/man-1.jpg'},
-  //   { name: 'Michael T.', from: 'Kinvara, G', availableSpace: '1/1', image: 'assets/images/man-2.jpg'},
-  //   { name: 'Sarah W.', from: 'Loughrea, G', availableSpace: '3/3', image: 'assets/images/woman-4.jpg'},
-  //   { name: 'Rose W.', from: 'Oranmore, G', availableSpace: '2/4', image: 'assets/images/woman-5.jpg'},
-  // ];
   constructor(private router: Router, @Inject(DATABASE_SERVICE_TOKEN) private databaseInterface: IDatabaseInterface) {}
 
   ngOnInit(): void {
@@ -50,6 +30,12 @@ export class HomePage implements OnInit{
 
   navigateToProfile(){
     this.router.navigate(['/profile']);
+  }
+
+  displayInwardRides(){  
+      this.makeHomepageContentsVisible = false;
+      const inwardsRides = this.databaseInterface.getRides('To Campus');
+      console.log('Inwards Rides:', inwardsRides);
   }
 
 }
